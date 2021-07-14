34243705e3abe1266a27587746105c611eccebcb
Erika Francesconi <fiorediren@gmail.com>
2021-07-14 21:04:55 +0200

    Better tests with testing-library-react-hooks

--- __tests__/useModal.test.js
+++ __tests__/useModal.test.js
@@ -1,36 +1,13 @@
 import { test, expect } from "@jest/globals";
-import { render } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react-hooks";
 
 import { useModal } from "hooks/useModal";
 
-const TestComponent = ({ firstContent, secondContent }) => {
-  const { openModal, modalContent, appendContent } = useModal();
+test("appends content correctly with hooks", async () => {
+  const { result } = renderHook(() => useModal());
+  const { openModal, appendContent } = result.current;
 
-  const setFirstContent = () => {
-    openModal("Title", [firstContent]);
-  };
-  const setSecondContent = () => {
-    appendContent([secondContent]);
-  };
-
-  return (
-    <div>
-      <button data-testid="button1" onClick={setFirstContent}></button>
-      <button data-testid="button2" onClick={setSecondContent}></button>
-      <div data-testid="content">{modalContent}</div>
-    </div>
-  );
-};
-
-test("appends content correctly", async () => {
-  const cmp = render(
-    <TestComponent firstContent="test1" secondContent="test2" />
-  );
-  (await cmp.findByTestId("button1")).click();
-
-  const content = await cmp.findByTestId("content");
-  expect(content.textContent).toBe("test1");
-
-  (await cmp.findByTestId("button2")).click();
-  expect(content.textContent).toBe("test1test2");
+  act(() => openModal("Title", ["test1"]));
+  act(() => appendContent(["test2"]));
+  expect(result.current.modalContent).toEqual(["test1", "test2"]);
 });
