1f0e6b1815d87865f15813ebbbfd1086fe2da2d7
Erika Francesconi <fiorediren@gmail.com>
2021-09-09 20:16:12 +0200

    Add Volumes Widget to Dashboard

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,14 +3,15 @@ import Link from "next/link";
 
 import { Layout } from "components";
 
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, Flex } from "@chakra-ui/react";
 import { CheckCircleIcon, LinkIcon, WarningIcon } from "@chakra-ui/icons";
 import { useState, useEffect } from "react";
 
 const Home = () => {
   const [containers, setContainers] = useState({ running: 0, stopped: 0 });
+  const [volumes, setVolumes] = useState({ used: 0, dangling: 0 });
 
-  const { listContainers } = useDocker();
+  const { listContainers, listVolumes } = useDocker();
 
   const fetchData = async () => {
     try {
@@ -18,6 +19,11 @@ const Home = () => {
       const running = res.filter((row) => row.State !== "exited").length;
       const stopped = res.filter((row) => row.State === "exited").length;
       setContainers({ running, stopped });
+
+      const resv = await listVolumes();
+      const used = resv.used.length;
+      const dangling = resv.dangling.length;
+      setVolumes({ used, dangling });
     } catch (error) {
       console.error(error);
     }
@@ -30,29 +36,68 @@ const Home = () => {
 
   return (
     <Layout title="Dashboard">
-      <Box
-        maxW="sm"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        p={6}
-      >
-        <Heading as="h3" size="md" color="blue.400" mb="2" fontWeight="normal">
-          <Link href="/docker/containers">
-            <a>
-              Containers <LinkIcon w={4} h={4} />
-            </a>
-          </Link>
-        </Heading>
-        <Text mt="1">
-          <CheckCircleIcon color="green" mr="2" />
-          {containers.running} running
-        </Text>
-        <Text mt="1">
-          <WarningIcon color="red" mr="2" />
-          {containers.stopped} stopped
-        </Text>
-      </Box>
+      <Flex>
+        <Box
+          w="300px"
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+          p={6}
+          mr={4}
+        >
+          <Heading
+            as="h3"
+            size="md"
+            color="blue.400"
+            mb="2"
+            fontWeight="normal"
+          >
+            <Link href="/docker/containers">
+              <a>
+                Containers <LinkIcon w={4} h={4} />
+              </a>
+            </Link>
+          </Heading>
+          <Text mt="1">
+            <CheckCircleIcon color="green" mr="2" />
+            {containers.running} running
+          </Text>
+          <Text mt="1">
+            <WarningIcon color="red" mr="2" />
+            {containers.stopped} stopped
+          </Text>
+        </Box>
+        <Box
+          w="300px"
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+          p={6}
+          mr={4}
+        >
+          <Heading
+            as="h3"
+            size="md"
+            color="blue.400"
+            mb="2"
+            fontWeight="normal"
+          >
+            <Link href="/docker/volumes">
+              <a>
+                Volumes <LinkIcon w={4} h={4} />
+              </a>
+            </Link>
+          </Heading>
+          <Text mt="1">
+            <CheckCircleIcon color="green" mr="2" />
+            {volumes.used} used
+          </Text>
+          <Text mt="1">
+            <WarningIcon color="red" mr="2" />
+            {volumes.dangling} dangling
+          </Text>
+        </Box>
+      </Flex>
     </Layout>
   );
 };
